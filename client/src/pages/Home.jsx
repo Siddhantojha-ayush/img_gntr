6a1bbfc99a09f18276e4f046986f65059a1c6506
Siddhant <siddhant612005@gmail.com>
2025-09-24 02:23:24 +0530

    Home pt-1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Headder from '../components/Headder'
+import Steps from '../components/Steps'
+import Description from '../components/Description.jsx'
+import Testimonials from '../components/Testimonials.jsx'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Headder/>
+      <Steps/>
+      <Description/>
+      <Testimonials/>
+    </div>
   )
 }
 
